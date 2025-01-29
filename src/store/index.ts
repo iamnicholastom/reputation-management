99881069288874413cac74cf824c11268c6efb09
Nicholas Tom <ntompg@gmail.com>
2025-01-29 15:49:38 +0530

    update store with reviewsReducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import reviewsReducer from "./features/reviews";
 
-export const store = configureStore({ reducer: {} });
+export const store = configureStore({
+  reducer: {
+    reviews: reviewsReducer,
+  },
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
