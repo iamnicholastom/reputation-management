f6ec9ae221f7bc0adadba5368ff362be02d34716
Nicholas Tom <ntompg@gmail.com>
2025-01-30 11:14:31 +0530

    create UI for adding reviews

---
+++ src/components/AddReview.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addReview } from "../store/features/reviews";
+
+const AddReview = () => {
+  const [content, setContent] = useState("");
+  const [rating, setRating] = useState(5);
+  const dispatch = useDispatch();
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    const review = { id: crypto.randomUUID(), content, rating };
+    dispatch(addReview(review));
+    setContent("");
+    setRating(5);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="description">Description</label>
+      <input
+        type="text"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        id="description"
+      />
+      <button>Submit Review</button>
+    </form>
+  );
+};
+
+export default AddReview;
