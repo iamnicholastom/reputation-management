25399d513c4cbb75a35350d589b57d62d1d0510f
Nicholas Tom <ntompg@gmail.com>
2025-01-30 16:57:04 +0530

    Setup TailwindCSS v4.0

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import ReviewList from "./components/ReviewList";
 function App() {
   return (
     <>
-      <h1>Review & Feedback Management</h1>
+      <h1 className="text-3xl font-bold underline">
+        Review & Feedback Management
+      </h1>
       <AddReview />
       <ReviewList />
     </>
